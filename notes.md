30f7d19b3d4476f455cfba1d0f5a878b9ff23656
Rich Gibson <Rich.Gibson@gmail.com>
2026-03-11 20:10:24 -0700

    added web map, added text,cleaned up

---
+++ notes.md
@@ -0,0 +1,13 @@
+# misc notes/links
+
+[relation vienna ring tram](https://www.openstreetmap.org/relation/150286#map=15/48.2095/16.3712&layers=TN)
+
+[online geocoder that works okay](https://www.geoapify.com/tools/geocoding-online)
+
+[overpass turbo - add an osm relation, it returns mappable data](https://overpass-turbo.eu/#)
+
+[Vienna Künsterlaus](https://en.wikipedia.org/wiki/Vienna_K%C3%BCnstlerhaus)
+
+[incredible vienna transit map from 2000](https://www.reddit.com/r/TransitDiagrams/comments/jdgea4/my_take_on_vienna_unified_transit_map/)
+
+[reddit TransitDiagrams](https://www.reddit.com/r/TransitDiagrams/)
---
+++ qgis2web_2023_06_15-15_01_29_860938/styles/vienna_paths_6_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_vienna_paths_6 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "26.0px \'.AppleSystemUIFont\', sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if (feature.get("name") !== null) {
+        labelText = String(feature.get("name"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(227,26,28,1.0)', lineDash: [10,5,1,5], lineCap: 'square', lineJoin: 'bevel', width: 4}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
